82608d6546cb8f54068b1ebc49d1a5c56f1727d9
Saminwza007 <s6452410016@sau.ac.th>
2023-08-23 16:46:13 +0700

    codev.1: create project and screen file...

---
+++ android/app/src/main/kotlin/com/example/dit_flutter_login_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dit_flutter_login_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:dit_flutter_login_01/views/login_a_ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    //wwidget หลักของแอปฯ
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginAUI(),
+    ),
+  );
+}
